732756b5748cce7637f3be51fb0f20906c5cc656
Fuzzbawls <fuzzbawls@gmail.com>
2017-07-23 16:27:46 -0700

    Icon and image file cleanup

--- share/qt/make_windows_icon.sh
+++ share/qt/make_windows_icon.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # create multiresolution windows icon
-ICON_DST=../../src/qt/res/icons/novacoin.ico
+ICON_DST=../../src/qt/res/icons/neutron.ico
 
-convert ../../src/qt/res/icons/novacoin-16.png ../../src/qt/res/icons/novacoin-32.png ../../src/qt/res/icons/novacoin-48.png ${ICON_DST}
+convert ../../src/qt/res/icons/neutron-16.png ../../src/qt/res/icons/neutron-32.png ../../src/qt/res/icons/neutron-48.png ${ICON_DST}
