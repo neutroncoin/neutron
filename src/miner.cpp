b6260d28057f71d0e3466505bbb1dc18061fbd0c
Eron Villarreal <eroninjapan@gmail.com>
2017-08-20 14:55:23 -0700

    Fix C++11 compiler compatibility

--- src/miner.cpp
+++ src/miner.cpp
@@ -356,7 +356,7 @@ CBlock* CreateNewBlock(CWallet* pwallet, bool fProofOfStake, int64_t* pFees)
         nLastBlockTx = nBlockTx;
         nLastBlockSize = nBlockSize;
         if (fDebug && GetBoolArg("-printpriority"))
-            printf("CreateNewBlock(): total size %"PRIu64"\n", nBlockSize);
+            printf("CreateNewBlock(): total size %" PRIu64 "\n", nBlockSize);
 
         if (!fProofOfStake)
             pblock->vtx[0].vout[0].nValue = GetProofOfWorkReward(nFees, pindexPrev->nHeight + 1);
