d0f10764db49856beb2ee84524098de67dc068b8
presstab <presstab1337@gmail.com>
2017-09-09 09:50:34 -0600

    Ask peers for blocks if its been 10 seconds since last block recvd.

--- src/main.h
+++ src/main.h
@@ -74,6 +74,7 @@ static const int64_t COIN_YEAR_REWARD = 5 * CENT; // 5% per year
 inline bool MoneyRange(int64_t nValue) { return (nValue >= 0 && nValue <= MAX_MONEY); }
 // Threshold for nLockTime: below this value it is interpreted as block number, otherwise as UNIX timestamp.
 static const unsigned int LOCKTIME_THRESHOLD = 500000000; // Tue Nov  5 00:53:20 1985 UTC
+static const int64_t MAX_TIME_SINCE_BEST_BLOCK = 10; // how many seconds to wait before sending next PushGetBlocks()
 
 #ifdef USE_UPNP
 static const int fHaveUPnP = true;
