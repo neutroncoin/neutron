22b49c8b89332692025f4e9d5b59e1e6c7ce492f
Eron Villarreal <eroninjapan@gmail.com>
2018-01-24 04:36:56 -0800

    Remove duplicate defines

--- src/main.h
+++ src/main.h
@@ -44,23 +44,6 @@ static const int64_t DARKSEND_COLLATERAL = (25000*COIN);
 static const int64_t DARKSEND_FEE = (0.0025000*COIN);
 static const int64_t DARKSEND_POOL_MAX = (250000.99*COIN);
 
-#define MASTERNODE_NOT_PROCESSED               0 // initial state
-#define MASTERNODE_IS_CAPABLE                  1
-#define MASTERNODE_NOT_CAPABLE                 2
-#define MASTERNODE_STOPPED                     3
-#define MASTERNODE_INPUT_TOO_NEW               4
-#define MASTERNODE_PORT_NOT_OPEN               6
-#define MASTERNODE_PORT_OPEN                   7
-#define MASTERNODE_SYNC_IN_PROCESS             8
-#define MASTERNODE_REMOTELY_ENABLED            9
-
-#define MASTERNODE_MIN_CONFIRMATIONS           15
-#define MASTERNODE_MIN_DSEEP_SECONDS           (30*60)
-#define MASTERNODE_MIN_DSEE_SECONDS            (5*60)
-#define MASTERNODE_PING_SECONDS                (1*60)
-#define MASTERNODE_EXPIRATION_SECONDS          (65*60)
-#define MASTERNODE_REMOVAL_SECONDS             (70*60)
-
 /** Maximum length of reject messages. */ // TODO: NTRN - move to validation.h eventually
 static const unsigned int MAX_REJECT_MESSAGE_LENGTH = 111;
 
