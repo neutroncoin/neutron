2a02ce9b01a8fe5c7a7f3c4023648e485e80b634
Eron Villarreal <eroninjapan@gmail.com>
2018-03-04 03:06:55 -0800

    Update SplashScreen class

--- src/qt/splashscreen.h
+++ src/qt/splashscreen.h
@@ -7,43 +7,25 @@
 
 #include <QSplashScreen>
 
-class NetworkStyle;
-
-/** Class for the splashscreen with information of the running client.
- *
- * @note this is intentionally not a QSplashScreen. Bitcoin Core initialization
- * can take a long time, and in that case a progress window that cannot be
- * moved around and minimized has turned out to be frustrating to the user.
+/** class for the splashscreen with information of the running client
  */
-class SplashScreen : public QWidget
+class SplashScreen : public QSplashScreen
 {
     Q_OBJECT
 
 public:
-    explicit SplashScreen(Qt::WindowFlags f, const NetworkStyle* networkStyle);
+    explicit SplashScreen(const QPixmap &pixmap, Qt::WindowFlags f);
     ~SplashScreen();
-
-protected:
-    void paintEvent(QPaintEvent* event);
-    void closeEvent(QCloseEvent* event);
 
 public slots:
     /** Slot to call finish() method as it's not defined as slot */
-    void slotFinish(QWidget* mainWin);
-
-    /** Show message and progress */
-    void showMessage(const QString& message, int alignment, const QColor& color);
+    void slotFinish(QWidget *mainWin);
 
 private:
     /** Connect core signals to splash screen */
     void subscribeToCoreSignals();
     /** Disconnect core signals to splash screen */
     void unsubscribeFromCoreSignals();
-
-    QPixmap pixmap;
-    QString curMessage;
-    QColor curColor;
-    int curAlignment;
 };
 
 #endif // BITCOIN_QT_SPLASHSCREEN_H
