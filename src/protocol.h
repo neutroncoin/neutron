863d5e1fdbfff0c6b52496ecb2c0f574f37f0a39
Eron Villarreal <eroninjapan@gmail.com>
2018-11-19 01:54:32 -0800

    Set name on ServiceFlags enum

--- src/protocol.h
+++ src/protocol.h
@@ -240,9 +240,12 @@ extern const char *DSEEP;
 const std::vector<std::string> &getAllNetMessageTypes();
 
 /** nServices flags */
-enum
-{
-    NODE_NETWORK = (1 << 0),
+enum ServiceFlags : uint64_t {
+    // Nothing
+    NODE_NONE = 0,
+    // NODE_NETWORK means that the node is capable of serving the complete block chain. It is currently
+    // set by all Bitcoin Core non pruned nodes, and is unset by SPV clients or other light clients.
+    NODE_NETWORK = (1 << 0)
 };
 
 /** A CService with information about it as peer */
