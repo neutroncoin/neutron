c1571860ab10ff8fce35c96a1c76373d3e25e50e
oicu8 <oicu8@neutroncoin.com>
2019-08-05 14:29:17 -0500

    update
    updating version protocols v301

--- src/version.h
+++ src/version.h
@@ -16,7 +16,7 @@ static const int DATABASE_VERSION = 70509;
 // network protocol versioning
 //
 
-static const int PROTOCOL_VERSION = 60019;
+static const int PROTOCOL_VERSION = 60020;
 
 // intial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 209;
@@ -27,6 +27,7 @@ static const int MIN_PEER_PROTO_VERSION_AFTER_V200_ENFORCEMENT = 60016;
 static const int MIN_PEER_PROTO_VERSION_AFTER_V201_ENFORCEMENT = 60017;
 static const int MIN_PEER_PROTO_VERSION_AFTER_V210_ENFORCEMENT = 60018;
 static const int MIN_PEER_PROTO_VERSION_AFTER_V3_ENFORCEMENT = 60019;
+static const int MIN_PEER_PROTO_VERSION_AFTER_V301_ENFORCEMENT = 60020;
 
 // nTime field added to CAddress, starting with this version;
 // if possible, avoid requesting addresses nodes older than this
