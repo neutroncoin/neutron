cc3302e82edf0926aa929c68c1bb435799917447
oicu8 <oicu8@neutroncoin.com>
2019-09-05 13:46:32 -0500

    Update main.cpp
    Updating Enforcement

--- src/main.cpp
+++ src/main.cpp
@@ -1490,11 +1490,14 @@ bool CBlock::ConnectBlock(CTxDB& txdb, CBlockIndex* pindex, bool fJustCheck)
         mapQueuedChanges[hashTx] = CTxIndex(posThisTx, tx.vout.size());
     }
 
-    // // enable mandatory checks
-    // if (!fEnforceMnWinner && (pindex->nHeight >= ENFORCE_MNWINNERS_HEIGHT)) {
-    //     if (fDebug) LogPrintf("\033[31mfEnforceMnWinner enabled\e[0m\n");
-    //     fEnforceMnWinner = true;
-    // }
+    // enable mandatory checks
+if (sporkManager.IsSporkActive(SPORK_2_MASTERNODE_WINNER_ENFORCEMENT)) {
+    fEnforceMnWinner = true;
+
+    if (fDebug) {
+        LogPrintf("fEnforceMnWinner enabled\n");
+    }
+}
 
     if (IsProofOfWork())
     {
