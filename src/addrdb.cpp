daae2caf53392b3ca1bf7c32cd98374aab01b1c0
Chaozer <chaozer@swippcoin.com>
2019-12-10 14:32:10 +0100

    Make sure debug messages in addrdb.cpp follow the usual format

--- src/addrdb.cpp
+++ src/addrdb.cpp
@@ -1,10 +1,11 @@
 // Copyright (c) 2009-2010 Satoshi Nakamoto
 // Copyright (c) 2009-2015 The Bitcoin Core developers
+// Copyright (c) 2015-2019 The Neutron Developers
+//
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #include "addrdb.h"
-
 #include "addrman.h"
 #include "chainparams.h"
 #include "clientversion.h"
@@ -28,86 +29,97 @@ bool CBanDB::Write(const banmap_t& banSet)
     GetRandBytes((unsigned char*)&randv, sizeof(randv));
     std::string tmpfn = strprintf("banlist.dat.%04x", randv);
 
-    // serialize banlist, checksum data up to that point, then append csum
+    // Serialize banlist, checksum data up to that point, then append csum
     CDataStream ssBanlist(SER_DISK, CLIENT_VERSION);
     ssBanlist << FLATDATA(pchMessageStart);
     ssBanlist << banSet;
     uint256 hash = Hash(ssBanlist.begin(), ssBanlist.end());
     ssBanlist << hash;
 
-    // open temp output file, and associate with CAutoFile
+    // Open temp output file, and associate with CAutoFile
     boost::filesystem::path pathTmp = GetDataDir() / tmpfn;
     FILE *file = fopen(pathTmp.string().c_str(), "wb");
     CAutoFile fileout(file, SER_DISK, CLIENT_VERSION);
+
     if (fileout.IsNull())
-        return error("%s: Failed to open file %s", __func__, pathTmp.string());
+        return error("%s : failed to open file %s", __func__, pathTmp.string());
 
     // Write and commit header, data
-    try {
+    try
+    {
         fileout << ssBanlist;
     }
-    catch (const std::exception& e) {
-        return error("%s: Serialize or I/O error - %s", __func__, e.what());
+    catch (const std::exception& e)
+    {
+        return error("%s : serialize or I/O error - %s", __func__, e.what());
     }
+
     FileCommit(fileout.Get());
     fileout.fclose();
 
-    // replace existing banlist.dat, if any, with new banlist.dat.XXXX
+    // Replace existing banlist.dat, if any, with new banlist.dat.XXXX
     if (!RenameOver(pathTmp, pathBanlist))
-        return error("%s: Rename-into-place failed", __func__);
+        return error("%s : rename-into-place failed", __func__);
 
     return true;
 }
 
 bool CBanDB::Read(banmap_t& banSet)
 {
-    // open input file, and associate with CAutoFile
+    // Open input file, and associate with CAutoFile
     FILE *file = fopen(pathBanlist.string().c_str(), "rb");
     CAutoFile filein(file, SER_DISK, CLIENT_VERSION);
-    if (filein.IsNull())
-        return error("%s: Failed to open file %s", __func__, pathBanlist.string());
 
-    // use file size to size memory buffer
+    if (filein.IsNull())
+        return error("%s : failed to open file %s", __func__, pathBanlist.string());
+
+    // Use file size to size memory buffer
     uint64_t fileSize = boost::filesystem::file_size(pathBanlist);
     uint64_t dataSize = 0;
+
     // Don't try to resize to a negative number if file is small
     if (fileSize >= sizeof(uint256))
         dataSize = fileSize - sizeof(uint256);
+
     std::vector<unsigned char> vchData;
     vchData.resize(dataSize);
     uint256 hashIn;
 
-    // read data and checksum from file
-    try {
+    try
+    {
         filein.read((char *)&vchData[0], dataSize);
         filein >> hashIn;
     }
-    catch (const std::exception& e) {
-        return error("%s: Deserialize or I/O error - %s", __func__, e.what());
+    catch (const std::exception& e)
+    {
+        return error("%s : deserialize or I/O error - %s", __func__, e.what());
     }
-    filein.fclose();
 
+    filein.fclose();
     CDataStream ssBanlist(vchData, SER_DISK, CLIENT_VERSION);
 
-    // verify stored checksum matches input data
+    // Verify stored checksum matches input data
     uint256 hashTmp = Hash(ssBanlist.begin(), ssBanlist.end());
+
     if (hashIn != hashTmp)
-        return error("%s: Checksum mismatch, data corrupted", __func__);
+        return error("%s : checksum mismatch, data corrupted", __func__);
 
     unsigned char pchMsgTmp[4];
-    try {
-        // de-serialize file header (network specific magic number) and ..
+    try
+    {
+        // De-serialize file header (network specific magic number) and ..
         ssBanlist >> FLATDATA(pchMsgTmp);
 
-        // ... verify the network matches ours
+        // ...verify the network matches ours
         if (memcmp(pchMsgTmp, pchMessageStart, sizeof(pchMsgTmp)))
-            return error("%s: Invalid network magic number", __func__);
+            return error("%s : invalid network magic number", __func__);
 
-        // de-serialize ban data
+        // De-serialize ban data
         ssBanlist >> banSet;
     }
-    catch (const std::exception& e) {
-        return error("%s: Deserialize or I/O error - %s", __func__, e.what());
+    catch (const std::exception& e)
+    {
+        return error("%s : deserialize or I/O error - %s", __func__, e.what());
     }
 
     return true;
@@ -125,71 +137,81 @@ bool CAddrDB::Write(const CAddrMan& addr)
     GetRandBytes((unsigned char*)&randv, sizeof(randv));
     std::string tmpfn = strprintf("peers.dat.%04x", randv);
 
-    // serialize addresses, checksum data up to that point, then append csum
+    // Serialize addresses, checksum data up to that point, then append csum
     CDataStream ssPeers(SER_DISK, CLIENT_VERSION);
     ssPeers << FLATDATA(pchMessageStart);
     ssPeers << addr;
     uint256 hash = Hash(ssPeers.begin(), ssPeers.end());
     ssPeers << hash;
 
-    // open temp output file, and associate with CAutoFile
+    // Open temp output file, and associate with CAutoFile
     boost::filesystem::path pathTmp = GetDataDir() / tmpfn;
     FILE *file = fopen(pathTmp.string().c_str(), "wb");
     CAutoFile fileout = CAutoFile(file, SER_DISK, CLIENT_VERSION);
+
     if (!fileout)
-        return error("CAddrman::Write() : open failed");
+        return error("%s : open failed", __func__);
 
     // Write and commit header, data
-    try {
+    try
+    {
         fileout << ssPeers;
     }
-    catch (const std::exception& e) {
-        return error("%s: Serialize or I/O error - %s", __func__, e.what());
+    catch (const std::exception& e)
+    {
+        return error("%s : serialize or I/O error - %s", __func__, e.what());
     }
+
     FileCommit(fileout);
     fileout.fclose();
 
-    // replace existing peers.dat, if any, with new peers.dat.XXXX
+    // Replace existing peers.dat, if any, with new peers.dat.XXXX
     if (!RenameOver(pathTmp, pathAddr))
-        return error("%s: Rename-into-place failed", __func__);
+        return error("%s : rename-into-place failed", __func__);
 
     return true;
 }
 
 bool CAddrDB::Read(CAddrMan& addr)
 {
-    // open input file, and associate with CAutoFile
+    // Open input file, and associate with CAutoFile
     FILE *file = fopen(pathAddr.string().c_str(), "rb");
     CAutoFile filein = CAutoFile(file, SER_DISK, CLIENT_VERSION);
-    if (!filein)
-        return error("CAddrman::Read() : open failed");
 
-    // use file size to size memory buffer
+    if (!filein)
+        return error("%s : open failed", __func__);
+
+    // Use file size to size memory buffer
     uint64_t fileSize = boost::filesystem::file_size(pathAddr);
     uint64_t dataSize = 0;
-    // Don't try to resize to a negative number if file is small
+
+    // Dont try to resize to a negative number if file is small
     if (fileSize >= sizeof(uint256))
         dataSize = fileSize - sizeof(uint256);
+
     std::vector<unsigned char> vchData;
     vchData.resize(dataSize);
     uint256 hashIn;
 
-    // read data and checksum from file
-    try {
+    // Read data and checksum from file
+    try
+    {
         filein.read((char *)&vchData[0], dataSize);
         filein >> hashIn;
     }
-    catch (const std::exception& e) {
-        return error("%s: Deserialize or I/O error - %s", __func__, e.what());
+    catch (const std::exception& e)
+    {
+        return error("%s : deserialize or I/O error - %s", __func__, e.what());
     }
-    filein.fclose();
 
+    filein.fclose();
     CDataStream ssPeers(vchData, SER_DISK, CLIENT_VERSION);
 
-    // verify stored checksum matches input data
+    // Verify stored checksum matches input data
     uint256 hashTmp = Hash(ssPeers.begin(), ssPeers.end());
+
     if (hashIn != hashTmp)
-        return error("%s: Checksum mismatch, data corrupted", __func__);
+        return error("%s : checksum mismatch, data corrupted", __func__);
 
     return Read(addr, ssPeers);
 }
@@ -197,21 +219,24 @@ bool CAddrDB::Read(CAddrMan& addr)
 bool CAddrDB::Read(CAddrMan& addr, CDataStream& ssPeers)
 {
     unsigned char pchMsgTmp[4];
-    try {
-        // de-serialize file header (network specific magic number) and ..
+
+    try
+    {
+        // De-serialize file header (network specific magic number) and ..
         ssPeers >> FLATDATA(pchMsgTmp);
 
-        // ... verify the network matches ours
+        // ...verify the network matches ours
         if (memcmp(pchMsgTmp, pchMessageStart, sizeof(pchMsgTmp)))
-            return error("%s: Invalid network magic number", __func__);
+            return error("%s : invalid network magic number", __func__);
 
-        // de-serialize address data into one CAddrMan object
+        // De-serialize address data into one CAddrMan object
         ssPeers >> addr;
     }
-    catch (const std::exception& e) {
-        // de-serialization has failed, ensure addrman is left in a clean state
+    catch (const std::exception& e)
+    {
+        // De-serialization has failed, ensure addrman is left in a clean state
         // addr.Clear();
-        return error("%s: Deserialize or I/O error - %s", __func__, e.what());
+        return error("%s : deserialize or I/O error - %s", __func__, e.what());
     }
 
     return true;
